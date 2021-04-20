e471d0d1de16f10c86ac5cb6d1917c371e621c0a
Podvolotskiy Arkadiy <Poarig@yandex.ru>
2021-04-20 18:22:34 +0300

    реализовал make_up_a_word и random_word

--- db_mod.py
+++ db_mod.py
@@ -11,11 +11,11 @@ class WordsDb:
         # Создание курсора
         self.cur = self.con.cursor()
 
-    def random_word(self, table_name):
+    def random_word(self):
         # получение списка всех id слов и выбор одного случайного
-        id_list = self.cur.execute("""SELECT id FROM ?""", (table_name,)).fetchall()
+        id_list = self.cur.execute("""SELECT id FROM english_words""").fetchall()
         id_word = random.choice(id_list)
         # получение слова и его переводов
-        result = self.cur.execute("""SELECT word, translations FROM words
-            WHERE id = ?""", (id_word, )).fetchall()
+        result = self.cur.execute("""SELECT word, translations FROM english_words
+            WHERE id = ?""", (*id_word, )).fetchone()
         return result
--- theory_func.py
+++ theory_func.py
@@ -1,9 +1,14 @@
+from db_mod import *
+
+
 def correct_translation(update, context):
     update.message.reply_text("test1")
 
 
 def random_word(update, context):
-    update.message.reply_text("test2")
+    # берём рандомное слово с переводом и выводим их через "-"
+    words_db = WordsDb()
+    update.message.reply_text(" - ".join(words_db.random_word()))
 
 
 def times_of_english(update, context):
